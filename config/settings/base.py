22a58303f425297b7724d03f852b6991cd277f98
oiudon <matsushimatoru63@yahoo.co.jp>
2024-10-24 21:17:50 +0900

    設定ファイル修正

--- config/settings/base.py
+++ config/settings/base.py
@@ -147,6 +147,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = "/var/www/jleague-ticket-price/static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
